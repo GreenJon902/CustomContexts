7a08b72fc03c9bea994def372e0c11d26dd305ad
greenjon902 <greenjon902@gmail.com>
2022-04-17 12:56:39 +0100

    added storing and loading for contexts

--- src/main/java/com/greenjon902/customcontexts/CustomContexts.java
+++ src/main/java/com/greenjon902/customcontexts/CustomContexts.java
@@ -1,25 +1,48 @@
 package com.greenjon902.customcontexts;
 
+import com.sun.tools.jdeprscan.scan.Scan;
 import net.luckperms.api.LuckPerms;
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.RegisteredServiceProvider;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.io.*;
+import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.Objects;
+import java.util.Scanner;
 import java.util.logging.Logger;
 
 public final class CustomContexts extends JavaPlugin {
     Logger logger = getLogger();
     CustomContextsCalculator customContextCalculator;
 
-    LuckPerms luckPermsApi;
+    public LuckPerms luckPermsApi;
 
     public ArrayList<String> contexts;
+
+    File contextsFile = new File(getDataFolder(), "contexts.txt").getAbsoluteFile();
 
     @Override
     public void onEnable() {
         logger.info("Starting CustomContexts...");
+
+        if (!contextsFile.exists()) {
+            try {
+                contextsFile.mkdirs();
+                contextsFile.createNewFile();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        Scanner scanner = new Scanner(contextsFile.getPath());
+        scanner.useDelimiter(",");
+
+        while (scanner.hasNext()) {
+            String value = scanner.next();
+            contexts.add(value);
+        }
 
         RegisteredServiceProvider<LuckPerms> provider = Bukkit.getServicesManager().getRegistration(LuckPerms.class);
         if (provider != null) {
@@ -38,6 +61,22 @@ public final class CustomContexts extends JavaPlugin {
 
     @Override
     public void onDisable() {
+        try {
+            contextsFile.mkdirs();
+
+            StringBuilder stringBuilder = new StringBuilder();
+            for (String context : contexts) {
+                stringBuilder.append(context);
+                stringBuilder.append(",");
+            }
+
+            FileWriter writer = new FileWriter(contextsFile);
+            writer.write(stringBuilder.toString());
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
         luckPermsApi.getContextManager().unregisterCalculator(customContextCalculator);
     }
 }
