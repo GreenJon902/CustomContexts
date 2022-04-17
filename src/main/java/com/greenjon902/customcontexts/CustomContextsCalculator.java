eb779fd003e1882406ec6dbae33bbdc76d9b13e8
greenjon902 <greenjon902@gmail.com>
2022-04-17 12:57:05 +0100

    added luckerperms calculator stuff

--- src/main/java/com/greenjon902/customcontexts/CustomContextsCalculator.java
+++ src/main/java/com/greenjon902/customcontexts/CustomContextsCalculator.java
@@ -1,18 +1,30 @@
 package com.greenjon902.customcontexts;
 
-import net.luckperms.api.context.ContextCalculator;
-import net.luckperms.api.context.ContextConsumer;
-import net.luckperms.api.context.ContextSet;
+import net.luckperms.api.context.*;
 import org.bukkit.entity.Player;
+import org.bukkit.plugin.java.JavaPlugin;
 import org.checkerframework.checker.nullness.qual.NonNull;
 
 public class CustomContextsCalculator implements ContextCalculator<Player> {
     @Override
     public void calculate(@NonNull Player target, @NonNull ContextConsumer consumer) {
+
+        for (String key : JavaPlugin.getPlugin(CustomContexts.class).contexts) {
+            String value = JavaPlugin.getPlugin(CustomContexts.class).luckPermsApi.getUserManager().getUser(
+                    target.getUniqueId()).getCachedData().getMetaData().getMetaValue("context." + key);
+            if (value != null) {
+                consumer.accept(key, value);
+            }
+        }
+
     }
 
     @Override
     public @NonNull ContextSet estimatePotentialContexts() {
-        return ContextCalculator.super.estimatePotentialContexts();
+        MutableContextSet set = MutableContextSet.create();
+        for (String key : JavaPlugin.getPlugin(CustomContexts.class).contexts) {
+            set.add(key, "");
+        }
+        return set;
     }
 }
